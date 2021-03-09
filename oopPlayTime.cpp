9a5121ab43b1c0d0773738214568c460ff0ae8e8
goncaloolas <goncalo.olas@gmail.com>
2021-03-09 19:36:20 +0000

    Building blocks and base UML Diagram

---
+++ oopPlayTime.cpp
@@ -0,0 +1,83 @@
+/*
+
+Example from somewhere... It will be gone soon
+
+// Create a Car class with some attributes
+class Car {
+  public:
+    string brand;   
+    string model;
+    int year;
+};
+
+int main() {
+  // Create an object of Car
+  Car carObj1;
+  carObj1.brand = "BMW";
+  carObj1.model = "X5";
+  carObj1.year = 1999;
+
+  // Create another object of Car
+  Car carObj2;
+  carObj2.brand = "Ford";
+  carObj2.model = "Mustang";
+  carObj2.year = 1969;
+
+  // Print attribute values
+  cout << carObj1.brand << " " << carObj1.model << " " << carObj1.year << "\n";
+  cout << carObj2.brand << " " << carObj2.model << " " << carObj2.year << "\n";
+  return 0;
+}
+
+
+*/
+
+
+#include <iostream>
+
+class customer
+{
+    public:
+        int cutWanted;
+        std::string name;
+        double payment;
+        int appointmentDay;
+};
+
+class barber
+{
+    public:
+        std::string name;
+        int age;
+        bool cutFinished = false;
+    
+    private:
+        double moneyMade;
+        int salary;
+        bool receivedSalary;
+};
+
+class receptionist
+{
+    public:
+        int day;
+        std::string name;
+        int age;
+        int cutWanted;
+        int invoiceNumber;
+        bool hasPayed = false;
+    
+    private:
+        int salary;
+        bool receivedSalary;
+
+};
+
+int main()
+{
+    customer john;
+    john.name = "John";
+
+    std::cout<<john.name;
+    return 0;
+}
